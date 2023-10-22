72a74e7142a3e12deb30a51d96499fb98977f7e9
VeenaSanthosh <148562608+VeenaSanthosh@users.noreply.github.com>
2023-10-22 10:28:56 +0530

    Add files via upload

---
+++ Eyemouse.py
@@ -0,0 +1,40 @@
+import cv2
+import mediapipe as mp
+import pyautogui
+screen_w,screen_h=pyautogui.size()
+
+face_mesh=mp.solutions.face_mesh.FaceMesh(refine_landmarks=True)
+cam=cv2.VideoCapture(0)
+
+while True:
+    _, frame=cam.read()
+    frame=cv2.flip(frame,1)
+    rgb_frame=cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+    output=face_mesh.process(rgb_frame)
+    landmark_points=output.multi_face_landmarks
+    #print(landmark_points)
+    frame_h,frame_w,_=frame.shape
+    if landmark_points:
+        landmarks=landmark_points[0].landmark
+        for id,landmark in enumerate(landmarks[474:478]):
+            x=int(landmark.x*frame_w)
+            y=int(landmark.y*frame_h)
+            cv2.circle(frame,(x,y),3,(255,0,0))
+            #print(x,y)
+            if id==1:
+                screen_x=screen_w/frame_w*x
+                screen_y=screen_h/frame_h*y
+                pyautogui.moveTo(screen_x,screen_y)
+        left=[landmarks[145],landmarks[159]]
+        for landmark in left:
+            x=int(landmark.x*frame_w)
+            y=int(landmark.y*frame_h)
+            cv2.circle(frame,(x,y),3,(255,0,255))
+        if(left[0].y-left[1].y)<0.008:
+            pyautogui.click()   
+            pyautogui.sleep(1)
+        #else:
+           #print('Your eyes are open')
+    cv2.imshow('Eyemouse',frame)
+    cv2.waitKey(1)  
+
